8aa30220b8621a00f6cf43d78ad51be8ab40deaa
jbravon <jonathanbravo@outlook.com>
2021-05-19 22:57:25 +0200

    Commit: Develop 03

--- dea_api/finder/queries.py
+++ dea_api/finder/queries.py
@@ -1,26 +1,17 @@
-import requests as req
-import json
 import os
-from .models import DEA
-from requests.api import get
-
-#? Carga de los datos del json
-# url = "http://datos.comunidad.madrid/catalogo/dataset/35609dd5-9430-4d2e-8198-3eeb277e5282/resource/c38446ec-ace1-4d22-942f-5cc4979d19ed/download/desfibriladores_externos_fuera_ambito_sanitario.json"
-# response = req.get(url).json()
-# with open ("deas.json", "w", encoding="utf8") as file:
-#     json.dump(response, file,   ensure_ascii=False, indent=4)
-
+import json
+from .models  import DEA
+import utm
 di_path = os.path.realpath(__file__)[0:-10]
-# print(di_path)
+
+
 
 def get_data():
     with open(f"{di_path}deas.json", encoding="utf8") as file:
         return json.load(file)
 
 data = get_data()["data"]
-print(len(data))
 
-#? INSERCION EN LA BD
 def insert_into(dataset):
     for dea in dataset:
         codigo_dea = dea["codigo_dea"]
@@ -28,8 +19,8 @@ def insert_into(dataset):
         direccion_via_nombre = dea["direccion_via_nombre"]
         direccion_portal_numero = dea["direccion_portal_numero"]
         horario_acceso = dea["horario_acceso"]
-        x_utm= dea["direccion_coordenada_x"]
-        y_utm= dea["direccion_coordenada_y"]
+        x_utm = dea["direccion_coordenada_x"]
+        y_utm = dea["direccion_coordenada_y"]
 
         DEA.objects.create(
             codigo_dea = codigo_dea,
@@ -40,3 +31,34 @@ def insert_into(dataset):
             x_utm = x_utm,
             y_utm = y_utm
         )
+
+class Dea:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def calculate_distance(self, user_x, user_y):
+        result = ((user_x - self.x)**2 + (user_y - self.y)**2)**0.5 
+        return result 
+#44776165
+
+def nearest_dea(user_lat, user_long, dataset):
+    user_xy = utm.from_latlon(user_lat, user_long)
+    first_dea = list(dataset)[0]
+    result = first_dea
+    first_object = Dea(first_dea.x_utm, first_dea.y_utm)
+    distance_to_beat = first_object.calculate_distance(user_xy[0], user_xy[1])
+
+    for dea in dataset:
+        dea_object = Dea(dea.x_utm, dea.y_utm)
+        distance = dea_object.calculate_distance(user_xy[0], user_xy[1])
+        if distance <= distance_to_beat:
+            result = dea
+            distance_to_beat = distance
+        else:
+            continue
+    print(result.y_utm)
+
+    dea_latlng = utm.to_latlon(result.x_utm,result.y_utm, 30, "N")
+    url = f"https://www.google.com/maps/dir/{user_lat},{user_long}/{dea_latlng[0]},{dea_latlng[1]} "
+    return result, url
--- dea_api/finder/views.py
+++ dea_api/finder/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from numpy import result_type
 from .queries import *
+from .models import DEA
 
 # Create your views here.
+
 def index(request):
-    # insert_into(data)
+    deas = DEA.objects.all()
+    dea, url = nearest_dea(40.4600052, -3.6285832, deas)
+    context = {"dea": dea, "url": url}
+    return render(request, "finder/index.html", context)
+
+
+def listado(request):
+    # insert_into(data) #Importacion de la BD
     # return HttpResponse("Se encuentra en la página Index")
-    return render(request, "finder/index.html")
+    deas = DEA.objects.all()
+    context = {"deas": deas}
+    return render(request, "finder/listado.html", context)
+
 
 def contact(request):
     return HttpResponse("Se encuentra en la página de contactos")
